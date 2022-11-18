faf6525213382c75c7ef35fd31d510fcceba3896
Ayagaki <1450057425@qq.com>
2022-11-18 11:38:03 +0800

    add cmsdk test

---
+++ src/test/java/CMSDKTest/api_test.java
@@ -0,0 +1,13 @@
+package CMSDKTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class api_test {
+    @Test
+    public void test1() {
+
+    }
+}
