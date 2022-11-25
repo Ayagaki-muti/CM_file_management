bc4551a653a36c79e01b57898686be28c9297f78
Ayagaki <1450057425@qq.com>
2022-11-25 10:30:45 +0800

    add config file

--- src/main/java/com/springboot/utils/chainmakerSDK/CMSDK.java
+++ src/main/java/com/springboot/utils/chainmakerSDK/CMSDK.java
@@ -16,39 +16,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
+import static com.springboot.utils.chainmakerSDK.StaticConfig.*;
+
 public class CMSDK {
-    static String SDK_CONFIG_PATH = "crypto-config/sdk_config.yml";
-    static final String ADMIN1_KEY_PATH = "crypto-config/TestCMorg1/user/admin1/示例测试用户1.sign.key";
-    static final String ADMIN1_CERT_PATH = "crypto-config/TestCMorg1/user/admin1/示例测试用户1.sign.crt";
-    static final String ADMIN2_KEY_PATH = "crypto-config/TestCMorg2/user/admin1/示例测试用户2.sign.key";
-    static final String ADMIN2_CERT_PATH = "crypto-config/TestCMorg2/user/admin1/示例测试用户2.sign.crt";
-    static final String ADMIN3_KEY_PATH = "crypto-config/TestCMorg3/node/consesus1/示例测试节点3.sign.key";
-    static final String ADMIN3_CERT_PATH = "crypto-config/TestCMorg3/node/consesus1/示例测试节点3.sign.crt";
-    static final String ADMIN4_KEY_PATH = "crypto-config/TestCMorg4/node/consesus1/示例测试节点4.sign.key";
-    static final String ADMIN4_CERT_PATH = "crypto-config/TestCMorg4/node/consesus1/示例测试节点4.sign.crt";
-    static String ADMIN1_TLS_KEY_PATH = "crypto-config/TestCMorg1/user/admin1/示例测试用户1.tls.key";
-    static String ADMIN1_TLS_CERT_PATH = "crypto-config/TestCMorg1/user/admin1/示例测试用户1.tls.crt";
-    static String ADMIN2_TLS_KEY_PATH = "crypto-config/TestCMorg2/user/admin1/示例测试用户2.tls.key";
-    static String ADMIN2_TLS_CERT_PATH = "crypto-config/TestCMorg2/user/admin1/示例测试用户2.tls.crt";
-    static String ADMIN3_TLS_KEY_PATH = "crypto-config/TestCMorg3/user/admin1/示例测试用户3.tls.key";
-    static String ADMIN3_TLS_CERT_PATH = "crypto-config/TestCMorg3/user/admin1/示例测试用户3.tls.crt";
-    static String ADMIN4_TLS_KEY_PATH = "crypto-config/TestCMorg4/user/admin1/示例测试用户4.tls.key";
-    static String ADMIN4_TLS_CERT_PATH = "crypto-config/TestCMorg4/user/admin1/示例测试用户4.tls.crt";
-
-    static final String ORG_ID1 = "TestCMorg1";
-    static final String ORG_ID2 = "TestCMorg2";
-    static final String ORG_ID3 = "TestCMorg3";
-    static final String ORG_ID4 = "TestCMorg4";
-
     private String chainCodeName; //智能合约名字
     private static final String CONTRACT_NAME = "mychannel";
 
     static ChainClient chainClient;
     static ChainManager chainManager;
-    static User adminUser1;
-    static User adminUser2;
-    static User adminUser3;
-    static User adminUser4;
 
     /**
      * 初始化长安链
